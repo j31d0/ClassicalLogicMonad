62c2a0443b30f2e7c9d650efb109a0f6fca87ecb
Jihee Park <j31d0@users.noreply.github.com>
2022-10-20 20:07:25 +0900

    Rename

--- proof.v
+++ proof.v
@@ -1,68 +1,89 @@
 Definition Lem := (forall Q: Prop, Q \/ ~Q).
 
-Definition C' (P: Prop) := Lem -> P.
+Definition C (P: Prop) := Lem -> P.
 
-Inductive C: Prop -> Prop :=
- | o_unit: forall P: Prop, P -> C P
- | o_mp_inv: forall (O: Prop -> Prop), (forall P: Prop, C (O P)) -> C (forall P: Prop, O P)
- | o_mp_inv2: forall (P Q: Prop), (P -> C Q) -> C (P -> Q)
- | o_fmapo: forall P Q: Prop, C P -> (P -> C Q) -> C Q
- | o_em: forall P: Prop, C (P \/ ~P)
-.
+Lemma c_unit: forall P: Prop, P -> C P.
+Proof.
+    intros P vP LEM; assumption.
+Qed.
 
-Ltac rclassic_0 H X Y Z := let a := fresh "H" in pose proof (o_mp_inv2 _ _ H) as a;
-apply (o_fmapo _ Z) in a; [ assumption | ]; let b := fresh "H" in
+Lemma c_bind: forall P Q: Prop, C P -> (P -> C Q) -> C Q.
+Proof.
+    intros P Q vCP HPQ LEM.
+    apply (HPQ (vCP LEM) LEM).
+Qed.
+
+Lemma c_prop1: forall (f: Prop -> Prop), (forall P: Prop, C (f P)) -> C (forall P: Prop, f P).
+Proof.
+    intros f H LEM P.
+    apply (H P LEM).
+Qed.
+
+Lemma c_prop2: forall (P Q: Prop), (P -> C Q) -> C (P -> Q).
+Proof.
+    intros P Q HPQ LEM vP.
+    apply (HPQ vP LEM).
+Qed.
+
+Lemma c_lem: forall P: Prop, C (P \/ ~P).
+Proof.
+    intros P LEM.
+    apply (LEM P).
+Qed.
+
+Ltac cremove_0 H X Y Z := let a := fresh "H" in pose proof (c_prop2 _ _ H) as a;
+apply (c_bind _ Z) in a; [ assumption | ]; let b := fresh "H" in
 intro b; move b before H; clear H; clear a; rename b into H.
 
-Ltac rclassic_1 H X Y Z := 
+Ltac cremove_1 H X Y Z := 
     let a := fresh "H" in assert (X -> Y) as a;
-    [ let b := fresh "H" in intro b; apply (o_unit X) in b; apply H in b; assumption
+    [ let b := fresh "H" in intro b; apply (c_unit X) in b; apply H in b; assumption
      | clear H; rename a into H ].
 
-Ltac rclassic_2 H X Y :=
-    apply (o_fmapo _ Y) in H; [assumption | ]; let a := fresh "H" in
+Ltac cremove_2 H X Y :=
+    apply (c_bind _ Y) in H; [assumption | ]; let a := fresh "H" in
     intro a; move a before H; clear H; rename a into H.
 
 
-Lemma o_hyp: forall P Q: Prop, C (P -> Q) -> C P -> C Q.
+Lemma cremove_hyp: forall P Q: Prop, C (P -> Q) -> C P -> C Q.
 Proof.
     intros P Q H1 H2.
-    rclassic_2 H1 (P -> Q) Q.
-    rclassic_2 H2 P Q.
-    apply H1 in H2. apply o_unit; assumption.
+    cremove_2 H1 (P -> Q) Q.
+    cremove_2 H2 P Q.
+    apply H1 in H2. apply c_unit; assumption.
 Qed.
 
-Ltac rclassic_3 H X Y :=
-    let a := fresh "H" in assert (a := o_hyp X Y H); move a before H; clear H; rename a into H
+Ltac cremove_3 H X Y :=
+    let a := fresh "H" in assert (a := cremove_hyp X Y H); move a before H; clear H; rename a into H
 .
 
-Ltac rclassic := repeat lazymatch goal with
+Ltac cremove := repeat lazymatch goal with
  | [ H: ?X -> C ?Y |- C ?Z ] =>
-    rclassic_0 H X Y Z
+    cremove_0 H X Y Z
  | [ H: C ?X -> ?Y |- C ?Z ] =>
-    rclassic_1 H X Y Z
+    cremove_1 H X Y Z
  | [ H: C ?X |- C ?Y ] =>
-    rclassic_2 H X Y
+    cremove_2 H X Y
  | [ |- C (?X -> ?Y) ] =>
-    apply (o_mp_inv X Y)
+    apply (c_prop1 X Y)
  | [ |- ?X -> _ ] => intro
  | [ H: C (?X -> ?Y) |- _ ] =>
-    rclassic_3 H X Y
+    cremove_3 H X Y
 end.
 
 Ltac cassumption := lazymatch goal with
- | [ H: ?X |- C ?X ] => apply o_unit; assumption
+ | [ H: ?X |- C ?X ] => apply c_unit; assumption
 end.
 
-Ltac capply H := apply (o_unit _ H).
-Ltac decided := apply o_unit.
+Ltac capply H := apply (c_unit _ H).
+Ltac decided := apply c_unit.
 
-Ltac rdestruct H := rclassic; destruct H.
+Ltac cdestruct H := cremove; destruct H.
 Lemma o_and: forall P Q: Prop, C P /\ C Q -> C (P /\ Q).
 Proof.
     intros P Q H.
     destruct H as [ H0 H1 ].
-    rclassic.
+    cremove.
     decided.
     apply (conj H0 H1).
 Qed.
@@ -72,27 +93,14 @@ Ltac csplit := lazymatch goal with
  | [ |- C (?A /\ ?B) ] => apply (o_and A B); split
 end.
 
-Ltac exm P := let a := fresh "H" in pose proof (o_em P) as a;
-  let b := type of a in let c := lazymatch goal with | [ |- C ?Z ] => Z end in rclassic_2 a b c; destruct a.
-
-
-Lemma if_exm: forall P: Prop, (forall Q: Prop, Q \/ ~ Q) -> C P -> P.
-Proof.
-    intros P H C.
-    induction C.
-    assumption.
-    assumption.
-    assumption.
-    apply (H1 IHC).
-    apply (H P).
-Qed.
-
+Ltac exm P := let a := fresh "H" in pose proof (c_lem P) as a;
+  let b := type of a in let c := lazymatch goal with | [ |- C ?Z ] => Z end in cremove_2 a b c; destruct a.
 
 Lemma classic_false: (exists P: Prop, C P /\ ~P) -> C False.
 Proof.
     intros H.
     destruct H. destruct H.
-    rclassic. absurd x; assumption.
+    cremove. absurd x; assumption.
 Qed.
 
 Lemma classic_not: forall P: Prop, (P -> C False) -> C (~ P).
@@ -100,20 +108,20 @@ Proof.
     intros P H.
     exm P.
     apply H in H0.
-    rclassic; inversion H0.
+    cremove; inversion H0.
     cassumption.
 Qed.
 
 Lemma o_mp: forall P Q: Prop, C (P -> Q) -> C P -> C Q.
 Proof.
     intros P Q H1 H2.
-    rclassic. specialize (H1 H2). cassumption.
+    cremove. specialize (H1 H2). cassumption.
 Qed.
 
 Lemma o_fmapd: forall A B P: Prop, C (A \/ B) -> (A -> C P) -> (B -> C P) -> C P.
 Proof.
     intros A B P.
-    rclassic.
+    cremove.
     destruct H. capply (H0 H).
     capply (H1 H).
 Qed.
@@ -121,14 +129,14 @@ Qed.
 Lemma o_dfmapo: forall P A B: Prop, C P -> (P -> C (A \/ B)) -> C (A \/ B).
 Proof.
     intros P A B H1 H2.
-    rclassic.
+    cremove.
     capply (H2 H1).
 Qed.
 
 Lemma o_dfmapd: forall A B P Q: Prop, C (A \/ B) -> (A -> C (P \/ Q)) -> (B -> C (P \/ Q)) -> C (P \/ Q).
 Proof.
     intros A B P Q H1 H2 H3.
-    rclassic.
+    cremove.
     destruct H1.
     capply (H2 H).
     capply (H3 H).
@@ -137,14 +145,14 @@ Qed.
 Lemma o_fmapo_c: forall P Q: Prop, (P -> C Q) -> C P -> C Q.
 Proof.
 intros P Q H R.
-rclassic.
+cremove.
 capply (H R).
 Qed.
 
 Lemma  dis_elim: forall A B P: Prop, C (A \/ B) -> C (A -> P) -> C (B -> P) -> C P.
 Proof.
 intros A B P H0 H1 H2.
-rclassic.
+cremove.
 destruct H0. pose proof (H1 H). capply H0.
 pose proof (H2 H). capply H0.
 Qed.
@@ -152,21 +160,21 @@ Qed.
 Lemma o_red: forall P: Prop, C (C P) -> C P.
 Proof.
     intros P H.
-    rclassic.
-    cassumption.
+    cremove.
+    assumption.
 Qed.
 
 Lemma o_mp_inv': forall P Q: Prop, (C P -> C Q) -> C (P -> Q).
 Proof.
     intros P Q H1.
-    rclassic.
+    cremove.
     cassumption.
 Qed.
 
 Lemma c_mp: forall A B P Q: Prop, C (A \/ B) -> C (A -> P) -> C (B -> Q) -> C (P \/ Q).
 Proof.
     intros A B P Q D Cap Cbq.
-    rclassic.
+    cremove.
     destruct D.
     specialize (Cap H); capply (or_introl Q Cap).
     specialize (Cbq H); capply (or_intror P Cbq).
@@ -175,8 +183,8 @@ Qed.
 Lemma PP: forall P Q: Prop, (P -> Q) /\ (~P -> Q) -> C Q.
 Proof.
     intros P Q Hpq.
-    pose proof (o_em P).
-    rclassic. decided.
+    pose proof (c_lem P).
+    cremove. decided.
     destruct H.
     destruct Hpq as [ Hpq _ ]. apply (Hpq H).
     destruct Hpq as [ _ Hpq ]. apply (Hpq H).
@@ -251,46 +259,308 @@ Qed.
 Lemma not_all_ex_not_p: forall (O: Prop -> Prop) , ~(forall P: Prop, O P) -> C (exists P: Prop, ~ O P).
 Proof.
 intros O H.
-pose proof (not_all_not_ex _ (fun x => ~ O x)). rclassic. apply o_unit in H0.
-apply (o_hyp _ _ H0).
+pose proof (not_all_not_ex _ (fun x => ~ O x)). cremove. apply c_unit in H0.
+apply (cremove_hyp _ _ H0).
 apply classic_not.
 intro Hall.
 unfold not in H.
-apply o_unit in H.
-rclassic_3 H (forall n : Prop, O n) False.
-apply H. rclassic. apply o_mp_inv. intro n. specialize (Hall n).
+apply c_unit in H.
+cremove_3 H (forall n : Prop, O n) False.
+apply H. cremove. apply c_prop1. intro n. specialize (Hall n).
 apply NNPP in Hall. assumption.
 Qed.
 
-Lemma using_classical: forall A : Prop, ((forall P: Prop, C P -> P) -> A) -> C A.
-Proof.
-    intros A H.
-    exm (forall P: Prop, C P -> P).
-    decided. apply (H H0).
-    assert (C (exists P: Prop, ~ (C P -> P))).
-    apply not_all_ex_not_p. assumption.
-    rdestruct H1.
-    apply imply_to_and in H1.
-    rdestruct H1.
-    rclassic. absurd x; assumption.
+Inductive CI: Prop -> Prop :=
+ | ci_unit: forall P: Prop, P -> CI P
+ | ci_prop1: forall (f: Prop -> Prop), (forall P: Prop, CI (f P)) -> CI (forall P: Prop, f P)
+ | ci_prop2: forall (P Q: Prop), (P -> CI Q) -> CI (P -> Q)
+ | ci_bind: forall P Q: Prop, CI P -> (P -> CI Q) -> CI Q
+ | ci_lem: forall P: Prop, CI (P \/ ~P)
+.
+
+
+
+Lemma if_exm: forall P: Prop, (forall Q: Prop, Q \/ ~ Q) -> CI P -> P.
+  Proof.
+      intros P H CI.
+      induction CI.
+      assumption.
+      assumption.
+      assumption.
+      apply (H1 IHCI).
+      apply (H P).
 Qed.
 
-Lemma using_exm: forall A: Prop, (Lem -> A) -> C A.
+Ltac ciremove_0 H X Y Z := let a := fresh "H" in pose proof (ci_prop2 _ _ H) as a;
+apply (ci_bind _ Z) in a; [ assumption | ]; let b := fresh "H" in
+intro b; move b before H; clear H; clear a; rename b into H.
+
+Ltac ciremove_1 H X Y Z := 
+    let a := fresh "H" in assert (X -> Y) as a;
+    [ let b := fresh "H" in intro b; apply (ci_unit X) in b; apply H in b; assumption
+     | clear H; rename a into H ].
+
+Ltac ciremove_2 H X Y :=
+    apply (ci_bind _ Y) in H; [assumption | ]; let a := fresh "H" in
+    intro a; move a before H; clear H; rename a into H.
+
+
+Lemma ciremove_hyp: forall P Q: Prop, CI (P -> Q) -> CI P -> CI Q.
+Proof.
+    intros P Q H1 H2.
+    ciremove_2 H1 (P -> Q) Q.
+    ciremove_2 H2 P Q.
+    apply H1 in H2. apply ci_unit; assumption.
+Qed.
+
+Ltac ciremove_3 H X Y :=
+    let a := fresh "H" in assert (a := ciremove_hyp X Y H); move a before H; clear H; rename a into H
+.
+
+Ltac ciremove := repeat lazymatch goal with
+ | [ H: ?X -> CI ?Y |- CI ?Z ] =>
+    ciremove_0 H X Y Z
+ | [ H: CI ?X -> ?Y |- CI ?Z ] =>
+    ciremove_1 H X Y Z
+ | [ H: CI ?X |- CI ?Y ] =>
+    ciremove_2 H X Y
+ | [ |- CI (?X -> ?Y) ] =>
+    apply (ci_prop1 X Y)
+ | [ |- ?X -> _ ] => intro
+ | [ H: CI (?X -> ?Y) |- _ ] =>
+    ciremove_3 H X Y
+end.
+
+Ltac ciassumption := lazymatch goal with
+ | [ H: ?X |- CI ?X ] => apply ci_unit; assumption
+end.
+
+Ltac ciapply H := apply (ci_unit _ H).
+Ltac cidecided := apply ci_unit.
+
+Ltac cidestruct H := ciremove; destruct H.
+Lemma oi_and: forall P Q: Prop, CI P /\ CI Q -> CI (P /\ Q).
+Proof.
+    intros P Q H.
+    destruct H as [ H0 H1 ].
+    ciremove.
+    cidecided.
+    apply (conj H0 H1).
+Qed.
+
+
+Ltac cisplit := lazymatch goal with
+ | [ |- CI (?A /\ ?B) ] => apply (oi_and A B); split
+end.
+
+Ltac ciexm P := let a := fresh "H" in pose proof (ci_lem P) as a;
+  let b := type of a in let c := lazymatch goal with | [ |- CI ?Z ] => Z end in ciremove_2 a b c; destruct a.
+
+Lemma ci_false: (exists P: Prop, CI P /\ ~P) -> CI False.
+Proof.
+    intros H.
+    destruct H. destruct H.
+    ciremove. absurd x; assumption.
+Qed.
+
+Lemma ci_not: forall P: Prop, (P -> CI False) -> CI (~ P).
+Proof.
+    intros P H.
+    ciexm P.
+    apply H in H0.
+    ciremove; inversion H0.
+    ciassumption.
+Qed.
+
+Lemma oi_mp: forall P Q: Prop, CI (P -> Q) -> CI P -> CI Q.
+Proof.
+    intros P Q H1 H2.
+    ciremove. specialize (H1 H2). ciassumption.
+Qed.
+
+Lemma oi_fmapd: forall A B P: Prop, CI (A \/ B) -> (A -> CI P) -> (B -> CI P) -> CI P.
+Proof.
+    intros A B P.
+    ciremove.
+    destruct H. ciapply (H0 H).
+    ciapply (H1 H).
+Qed.
+
+Lemma oi_dfmapo: forall P A B: Prop, CI P -> (P -> CI (A \/ B)) -> CI (A \/ B).
+Proof.
+    intros P A B H1 H2.
+    ciremove.
+    ciapply (H2 H1).
+Qed.
+
+Lemma oi_dfmapd: forall A B P Q: Prop, CI (A \/ B) -> (A -> CI (P \/ Q)) -> (B -> CI (P \/ Q)) -> CI (P \/ Q).
+Proof.
+    intros A B P Q H1 H2 H3.
+    ciremove.
+    destruct H1.
+    ciapply (H2 H).
+    ciapply (H3 H).
+Qed.
+
+Lemma oi_fmapo_c: forall P Q: Prop, (P -> CI Q) -> CI P -> CI Q.
+Proof.
+intros P Q H R.
+ciremove.
+ciapply (H R).
+Qed.
+
+Lemma  disi_elim: forall A B P: Prop, CI (A \/ B) -> CI (A -> P) -> CI (B -> P) -> CI P.
+Proof.
+intros A B P H0 H1 H2.
+ciremove.
+destruct H0. pose proof (H1 H). ciapply H0.
+pose proof (H2 H). ciapply H0.
+Qed.
+
+Lemma oi_red: forall P: Prop, CI (CI P) -> CI P.
+Proof.
+    intros P H.
+    ciremove.
+    ciassumption.
+Qed.
+
+Lemma oi_mp_inv': forall P Q: Prop, (CI P -> CI Q) -> CI (P -> Q).
+Proof.
+    intros P Q H1.
+    ciremove.
+    ciassumption.
+Qed.
+
+Lemma ci_mp: forall A B P Q: Prop, CI (A \/ B) -> CI (A -> P) -> CI (B -> Q) -> CI (P \/ Q).
+Proof.
+    intros A B P Q D Cap Cbq.
+    ciremove.
+    destruct D.
+    specialize (Cap H); ciapply (or_introl Q Cap).
+    specialize (Cbq H); ciapply (or_intror P Cbq).
+Qed.
+
+Lemma PPi: forall P Q: Prop, (P -> Q) /\ (~P -> Q) -> CI Q.
+Proof.
+    intros P Q Hpq.
+    pose proof (ci_lem P).
+    ciremove. cidecided.
+    destruct H.
+    destruct Hpq as [ Hpq _ ]. apply (Hpq H).
+    destruct Hpq as [ _ Hpq ]. apply (Hpq H).
+Qed.
+
+Lemma NNPPi: forall P: Prop, ~~P -> CI P.
+Proof.
+intros P Hnnp.
+ciexm P; cidecided.
+assumption.
+specialize (Hnnp H); inversion Hnnp.
+Qed.
+
+
+Lemma Peircei: forall P: Prop, ((P -> False) -> P) -> CI P.
+Proof.
+intros P H.
+ciexm P; cidecided. assumption.
+apply H in H0. assumption.
+Qed.
+
+Lemma not_imply_elimi: forall P Q: Prop, ~ (P -> Q) -> CI P.
+Proof.
+intros P Q H. apply NNPPi. red.
+intro. apply H. intro. absurd P; assumption.
+Qed.
+
+Lemma not_imply_elim2i: forall P Q: Prop, ~ (P -> Q) -> CI (~ Q).
+Proof.
+intros P Q H. ciexm Q; cidecided. assert False. apply H. intro; assumption.
+inversion H1. assumption.
+Qed.
+
+Lemma imply_to_ori: forall P Q: Prop, (P -> Q) -> CI (~ P \/ Q).
+Proof.
+intros P Q H. ciexm P; cidecided. right. apply (H H0).
+left. assumption.
+Qed.
+
+Lemma imply_to_andi: forall P Q: Prop, ~ (P -> Q) -> CI (P /\ ~ Q).
+Proof.
+intros P Q H.
+cisplit. apply (not_imply_elimi P Q H). apply (not_imply_elim2i P Q H).
+Qed.
+
+Lemma or_to_implyi: forall P Q: Prop, ~ P \/ Q -> P -> CI Q.
+Proof.
+intros P Q H H1.
+destruct H.
+absurd P; assumption.
+ciassumption.
+Qed.
+
+Lemma not_and_ori: forall P Q: Prop, ~ (P /\ Q) -> CI ( ~P \/ ~Q ).
+Proof.
+intros P Q H.
+ciexm P. ciexm Q. pose proof (conj H0 H1). absurd (P /\ Q); assumption.
+all: cidecided. right; assumption. left; assumption.
+Qed.
+
+Lemma not_all_not_exi: forall (U: Type) (P: U -> Prop) , ~(forall n: U, ~ P n) -> CI (exists n: U, P n).
+Proof.
+intros U P H.
+apply NNPPi.
+intro abs.
+apply H.
+intros n H2.
+apply abs; exists n; exact H2.
+Qed.
+
+
+Lemma not_all_ex_not_pi: forall (O: Prop -> Prop) , ~(forall P: Prop, O P) -> CI (exists P: Prop, ~ O P).
+Proof.
+intros O H.
+pose proof (not_all_not_exi _ (fun x => ~ O x)). ciremove. apply ci_unit in H0.
+apply (ciremove_hyp _ _ H0).
+apply ci_not.
+intro Hall.
+unfold not in H.
+apply ci_unit in H.
+ciremove_3 H (forall n : Prop, O n) False.
+apply H. ciremove. apply ci_prop1. intro n. specialize (Hall n).
+apply NNPPi in Hall. assumption.
+Qed.
+
+Lemma using_CI: forall A : Prop, ((forall P: Prop, CI P -> P) -> A) -> CI A.
 Proof.
     intros A H.
-    apply using_classical.
+    ciexm (forall P: Prop, CI P -> P).
+    cidecided. apply (H H0).
+    assert (CI (exists P: Prop, ~ (CI P -> P))).
+    apply not_all_ex_not_pi. assumption.
+    cidestruct H1.
+    apply imply_to_andi in H1.
+    cidestruct H1.
+    ciremove. absurd x; assumption.
+Qed.
+
+Lemma using_ciexm: forall A: Prop, (Lem -> A) -> CI A.
+Proof.
+    intros A H.
+    apply using_CI.
     intro Hctoi.
     assert (forall P: Prop, P \/ ~P).
     intro P.
-    pose proof (o_em P). specialize (Hctoi (P \/ ~P) H0). assumption.
+    pose proof (ci_lem P). specialize (Hctoi (P \/ ~P) H0). assumption.
     apply (H H0).
 Qed.
 
-Theorem equiv_exm: forall P: Prop, C P <-> C' P.
+
+  
+Theorem equiv_C_CI: forall P: Prop, C P <-> CI P.
 Proof.
     split; intro H.
-    intro Hexm. apply (if_exm _ Hexm H).
-    apply using_exm. assumption.
+    apply using_ciexm. assumption.
+    intro Hciexm. apply (if_exm _ Hciexm H).
 Qed.
 
 
@@ -351,30 +621,30 @@ assumption.
 Qed.
 
 
-Definition DNProp := (forall (O: Prop -> Prop), (forall Q: Prop, ~~ (O Q)) -> ~~ (forall Q: Prop, O Q)).
+Definition DNS := (forall (O: Prop -> Prop), (forall Q: Prop, ~~ (O Q)) -> ~~ (forall Q: Prop, O Q)).
 
-Definition DN (P: Prop) := DNProp -> ~~ P.
+Definition DN (P: Prop) := DNS -> ~~ P.
 
 Definition DNRel (P Q: Prop) := P = Q.
 
-Definition CDNProp : C DNProp.
+Definition CDNProp : CI DNS.
 Proof.
-apply using_classical.
+apply using_CI.
 intro Hctoi.
 intros O P.
 enough (forall Q: Prop, O Q).
 intro K. apply (K H).
 intro vQ.
 specialize (P vQ).
-apply (Hctoi _ (NNPP _ P)).
+apply (Hctoi _ (NNPPi _ P)).
 Qed.
 
-Lemma using_nn: forall A: Prop, DN A -> C A.
+Lemma using_nn: forall A: Prop, DN A -> CI A.
 Proof.
   intros A H.
-  apply using_classical.
+  apply using_CI.
   intro Hctoi.
-  apply (Hctoi _ (NNPP _ (H (Hctoi _ CDNProp)))).
+  apply (Hctoi _ (NNPPi _ (H (Hctoi _ CDNProp)))).
 Qed.
 
 Lemma DNLem: DN Lem .
@@ -385,7 +655,7 @@ intro P.
 apply notnot_lem.
 Qed.
 
-Lemma nn_using: forall A: Prop, C A ->
+Lemma nn_using: forall A: Prop, CI A ->
   DN A.
 Proof.
 intros A H HI. intro K.
@@ -395,7 +665,7 @@ apply (DNLem HI).
 Qed.
 
 
-Lemma equiv_dn: forall A: Prop, DN A <-> C A.
+Lemma equiv_DN_CI: forall A: Prop, DN A <-> CI A.
 Proof.
     intro A. split. apply using_nn. apply nn_using.
 Qed.
